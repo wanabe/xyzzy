5a9ee19f9a13c83e24f792d47c24d97a1c99770e
hoge <hoge@hoge>
2013-05-19 23:59:52 +0900

    mingw doesn't have WinSDKVer.h

--- src/targetver.h
+++ src/targetver.h
@@ -5,7 +5,9 @@
 // 以前の Windows プラットフォーム用にアプリケーションをビルドする場合は、WinSDKVer.h をインクルードし、
 // SDKDDKVer.h をインクルードする前に、サポート対象とするプラットフォームを示すように _WIN32_WINNT マクロを設定します。
 
+#ifndef __MINGW32__
 #include <WinSDKVer.h>
+#endif
 
 #ifndef _WIN32_WINNT
 #define _WIN32_WINNT 0x0502
